86cae33c1eb8c4dfad5ab22d64f8a600081717bc
Vincent <vincent@calmcode.io>
2023-12-05 17:24:03 +0100

    ci failures are a mystery for me. tu rning it off for now

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -23,13 +23,12 @@ def test_basic_interactions(query, base_call):
         page.get_by_label("New query for ANN:").click()
         page.get_by_label("New query for ANN:").type(query, delay=150)
         page.get_by_role("button", name="Refresh Stream").click()
-        time.sleep(2)
         page.get_by_text("Reset stream?").click()
         
         # Hit accept a few times, making sure that the query appears
         for _ in range(3):
             page.get_by_label("accept (a)").click()
-            time.sleep(1)
+            time.sleep(0.5)
             # We check the entire container because we're interested in the meta information.
             # The retreived text may not have a perfect match for the query, but the meta should!
             elem = page.locator(".prodigy-container").first
